6af66a895cffd0023289106c980648c11074484c
MOCodifyX <xmike.mox@gmail.com>
2025-05-21 14:27:14 -0300

    GitPage MS08

--- Semana_8_Front_End/M1S08/vite.config.ts
+++ Semana_8_Front_End/M1S08/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/FuturoDev_Senai/Semana_8_Front_End/M1S08/',
   plugins: [react()],
 })
